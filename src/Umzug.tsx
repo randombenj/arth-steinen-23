d769fa54464a95dc67810f74a3020dbf8e2c1490
Benj Fassbind <randombenj@gmail.com>
2023-09-04 22:56:51 +0200

    Fix build

--- src/Umzug.tsx
+++ src/Umzug.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import TimelineOppositeContent, {
