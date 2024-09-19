96934d75ab3babd08e7ce65819c785ce3fda7438
Benj Fassbind <randombenj@gmail.com>
2024-09-20 00:27:34 +0200

    Nicer implementation of resize handler

--- src/DigitalTimeguide.tsx
+++ src/DigitalTimeguide.tsx
@@ -1,10 +1,31 @@
 
+import { useEffect, useRef } from 'react';
 import MyFest from './MyFest';
 
 
 function DigitalTimeguide() {
+  const observedElementRef = useRef(null);
+
+  useEffect(() => {
+    if (observedElementRef.current) {
+      const observer = new ResizeObserver((entries) => {
+        // @ts-ignore
+        const height = observedElementRef.current.offsetHeight
+        // eslint-disable-next-line no-restricted-globals
+        parent.postMessage(`resize::${height}`, '*')
+      });
+
+      observer.observe(observedElementRef.current);
+
+      // Cleanup function
+      return () => {
+          observer.disconnect();
+      };
+    }
+  }, []);
+
   return (
-    <div id="container">
+    <div ref={observedElementRef}>
       <MyFest />
     </div>
   );
